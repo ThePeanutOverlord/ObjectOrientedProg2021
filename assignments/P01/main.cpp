85242aa435f99d3a0b4a8fa8d07a27e1133e6e30
paigechampagne <paigechamp@gmail.com>
2020-04-07 21:05:58 -0500

    gfdgfhjk

--- assignments/P01/main.cpp
+++ assignments/P01/main.cpp
@@ -1,44 +1,23 @@
-///////////////////////////////////////////////////////////////////////////////
-//                   
-// Author:           Paige Champagne
-// Email:            paigechamp@gmail.com
-// Label:            program 1
-// Title:            game of war
-// Course:           CMPS 2143
-// Semester:         Spring 2020
-//
-// Description:
-//          main file that contains game driver
-//
-// Usage:
-//       the part that actually does the thing
-//
-// Files:           main.cpp
-//                  playingcard.hpp
-//                  poker_game.hpp
-//                  termio.h
-/////////////////////////////////////////////////////////////////////////////////
-
 #include "poker_game.hpp"
 #include <iomanip>
 
 int main(){
 
     Term::IO io;
-    Game k; //instance of a game
-    Deck D(52); //deck of 52 cards
-    Hand player1(26); //hand for p1
-    Hand player2(26); //hand for p2
-    bool cont = true; //loop control variable
-    for(int i=0;i<50;i++){ //fills deck
+    Game k;
+    Deck D(52);
+    Hand player1(26);
+    Hand player2(26);
+    bool cont = true;
+    for(int i=0;i<50;i++){
         D.Shuffle();
         //D.Print(2,true);
         break;
-       // io << Term::sleep(200); 
+        io << Term::sleep(200); 
     }
 
-   D.Split(player1, player2); //splits deck between player hands
-   player1.Shuffle(); 
+   D.Split(player1, player2);
+   player1.Shuffle();
   // player1.Print(4, true);
    
  
@@ -46,17 +25,17 @@ int main(){
   // player2.Print(4, true); 
 while(cont){
   io << "Cards: " << player1.Size() << "     Cards: " << player2.Size() <<"\n";
-   CardContainer i(player1.Play(), player2.Play()); //puts first card of each into playing container
+   CardContainer i(player1.Play(), player2.Play());
     
-   i.Print(2, false); //prints cards being played
+   i.Print(2, false);
     io<<"Player 1        Player 2\n";
 
-    k.Distribute(k.Compare(i.Get(0), i.Get(1)), player1, player2, i); //compares and distributes cards
+    k.Distribute(k.Compare(i.Get(0), i.Get(1)), player1, player2, i);
      
-    if(player1.Size() == 0 || player2.Size() == 0){ //checks if either of them are out of cards
-     cont = k.End(player1.Size(), player2.Size());//ends game if either is out of cards
+    if(player1.Size() == 0 || player2.Size() == 0){
+     cont = k.End(player1.Size(), player2.Size());
     }
-
+ io << Term::sleep(200); 
 }
     return 0;
 }
